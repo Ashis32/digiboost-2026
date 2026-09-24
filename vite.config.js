476c9fca0b76d1f0affe52558d54cf91b247cee7
Ashis32 <ashiskumarbehura32@gmail.com>
2026-09-24 11:39:51 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import { fileURLToPath, URL } from 'url';
 const resolvePath = (relativePath) => fileURLToPath(new URL(relativePath, import.meta.url));
 
 export default defineConfig(({ command }) => {
-  const isGhPages = process.env.GITHUB_PAGES === '1' || process.env.GITHUB_PAGES === 'true';
   return {
-    base: isGhPages ? '/digiboost-2026/' : '/',
+    base: command === 'build' ? './' : '/',
     server: {
       port: 5173,
       host: true
